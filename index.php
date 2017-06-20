6f1e4d1919066b90ab074ef463ffb35bb3894484
HSO <>
2017-06-19 20:31:26 -0400

    Cambios para chequear

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        /*
+        Buen examen!!!
+         *          */
         ?>
     </body>
 </html>
